624f6d69773a30d5998a69725ed4ca8eea0d2155
sirx2713 <bradx2713@gmail.com>
2023-02-19 01:06:40 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,16 +10,16 @@ char c;
 char d;
 c = 'a';
 d = 'A';
-while 
-	(c <= 'z') {
-		putchar(c);
-		c++;
-	}
 while
-	(d <= 'Z') {
-		putchar(d);
-		d++;
-	}
+(c <= 'z') {
+putchar(c);
+c++;
+}
+while
+(d <= 'Z') {
+putchar(d);
+d++;
+}
 putchar('\n');
 return (0);
 }
