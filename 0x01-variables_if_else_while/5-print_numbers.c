2f3869f03ce98e5b9958ff35a78c5a1ebe36ef3f
sirx2713 <bradx2713@gmail.com>
2023-02-22 12:24:50 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
  * main - Entry point
  *
+ * Description: print all single digit numbers
+ *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
-int a;
-for (a = 0; a < 10; a++)
-	putchar(a);
-putchar('\n');
-return (0);
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		++digit;
+	}
+	printf("\n");
+
+	return (0);
 }
