cab2e14fa30752bff93a4bcb289ba1fb35f18b0f
sirx2713 <bradx2713@gmail.com>
2023-02-22 09:52:41 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,18 @@
 #include"main.h"
 
 /**
- * add - add two integers
+ * print_alphabet_x10 - function that will print the alphabet 10 times
  *
- * @a: first input
- * @b: second input
- *
- * Return: sum of a & b
 */
 
-int add(int a, int b)
+void print_alphabet_x10(void)
 {
-	int sum = a + b;
+	int line, ch;
 
-	return (sum);
+	for (line = 0; line <= 9; ++line)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
+			_putchar(ch);
+		_putchar('\n');
+	}
 }
